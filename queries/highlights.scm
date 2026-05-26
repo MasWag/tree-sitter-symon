4b47ff6079b93525b8f28ac5ac8158289dfbe5bd
Masaki Waga <masakiwaga@gmail.com>
2026-05-26 15:29:37 +0900

    format: rewrite highlights.scm with idiomdact style
    - Change line comments from ';' to '#'.
    - Expand array elements onto separate lines.
    - Change (number) @constant to @number.
    - Change (identifier) @variable.parameter to @variable.

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,40 +1,71 @@
-;; 1. Comments
-((line_comment) @comment)
+; 1. Comments
+(line_comment) @comment
 
-;; 2. Strings
-((string) @string)
+; 2. Strings
+(string) @string
 
-;; 3. Types
-([
- "string" "number" "param"
- ] @type)
+; 3. Types
+[
+  "string"
+  "number"
+  "param"
+] @type
 
-;; 4. Numeric literals
-((number) @constant)
+; 4. Numeric literals
+(number) @number
 
-;; 5. Keywords
-([
-  "var" "init" "signature" "expr" "within" "optional" "zero_or_more" "one_or_more" "one_of" "or" "all_of" "and" "ignore"
- ] @keyword)
+; 5. Keywords
+[
+  "var"
+  "init"
+  "signature"
+  "expr"
+  "within"
+  "optional"
+  "zero_or_more"
+  "one_or_more"
+  "one_of"
+  "or"
+  "all_of"
+  "and"
+  "ignore"
+] @keyword
 
-;; 6. Operators
-([
- "==" "!="
- "<" "<=" "=" "<>" ">" ">="
- "%" "*" "+" "-" "?"
- ":=" "|"
- "||" "&&"
- ] @operator)
+; 6. Operators
+[
+  "=="
+  "!="
+  "<"
+  "<="
+  "="
+  "<>"
+  ">"
+  ">="
+  "%"
+  "*"
+  "+"
+  "-"
+  "?"
+  ":="
+  "|"
+  "||"
+  "&&"
+] @operator
 
-;; 7. Brackets
-([
- "(" ")" "{" "}"
- ] @punctuation.bracket)
+; 7. Brackets
+[
+  "("
+  ")"
+  "{"
+  "}"
+] @punctuation.bracket
 
-;; 8. Delimiters
-([
- "," ";" ":"
- ] @punctuation.delimiter)
+; 8. Delimiters
+[
+  ","
+  ";"
+  ":"
+] @punctuation.delimiter
 
-;; 9. All other identifiers
-((identifier) @variable.parameter)
+; 9. All other identifiers
+(identifier) @variable
